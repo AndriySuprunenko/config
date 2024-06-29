2380cb69d4c9ffee2715116588c882ade363b9eb
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2024-06-29 20:24:21 +0300

    delete bufferline

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -48,8 +48,6 @@ return {
 				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 			},
 		})
-
-		-- setup must be called before loading
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
