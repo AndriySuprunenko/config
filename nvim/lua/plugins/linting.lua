1aedefdcebc0fabbcaf89cbbc9adb5ff56736c55
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2024-12-28 13:32:06 +0200

    linting

--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -13,6 +13,7 @@ return {
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
 			python = { "pylint", "ruff", "mypy" },
+			php = { "phpstan" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
