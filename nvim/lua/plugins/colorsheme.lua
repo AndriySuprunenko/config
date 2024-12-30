acdb2114a041ec6c1154aace0470b4fd13bcf512
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2024-12-30 15:27:16 +0200

    lazyvim

--- nvim/lua/plugins/colorsheme.lua
+++ nvim/lua/plugins/colorsheme.lua
@@ -1,16 +1,34 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      transparent = true,
-      styles = {
-        comments = { italic = true },
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    })
-    vim.cmd.colorscheme("tokyonight")
-  end,
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("solarized-osaka").setup({
+        transparent = true,
+        styles = {
+          comments = { italic = true },
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+      -- vim.cmd.colorscheme("solarized-osaka")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          comments = { italic = true },
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+      vim.cmd.colorscheme("tokyonight")
+    end,
+  },
 }
