5d7c2b8eade884798d0cfd7089366f4e96a1478c
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-01-21 20:34:59 +0200

    fix

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -6,11 +6,12 @@ for sid in $(aerospace list-workspaces --all); do
     sketchybar --add item space.$sid left \
         --subscribe space.$sid aerospace_workspace_change \
         --set space.$sid \
-        background.color=$ACCENT_COLOR \
+        background.color=$ITEM_BG_COLOR_ONE \
         background.corner_radius=5 \
         background.height=26 \
         background.drawing=off \
         label="$sid" \
+        label.color=$ITEM_BG_COLOR_TREE \
         icon.padding_left=0                          \
         icon.padding_right=14                          \
         label.padding_right=20                        \
