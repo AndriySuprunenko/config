a6a29372be15c8f0f4b917d6f80cec6084bd5c9b
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-03-15 13:46:55 +0200

    add japanese time format

--- sketchybar/plugins/calendar.sh
+++ sketchybar/plugins/calendar.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sketchybar --set $NAME label="$(date +'%a %d %b %H:%M')"
+# sketchybar --set $NAME label="$(date +'%a %d %b %H:%M')"
+sketchybar --set $NAME label="$(LANG=ja_JP.UTF-8 date +'%m月%d日 (%a) %H:%M')"
