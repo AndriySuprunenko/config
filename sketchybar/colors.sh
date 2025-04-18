854d2323c3eaf103ef202dc17f83469a186cea6c
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-04-18 13:07:46 +0300

    some commit

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -4,7 +4,8 @@ export TEXT_COLOR=0xff1e1e2e
 
 # -- Teal Scheme --
 # export BAR_COLOR=0xcc1f2335
-export BAR_COLOR=0xff1f2430
+# export BAR_COLOR=0xff1f2430
+export BAR_COLOR=0x801e2330
 
 export ITEM_BG_COLOR_ONE=0xff7aa2f7
 export ITEM_BG_COLOR_TWO=0xffff757f
--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -6,12 +6,12 @@ for sid in $(aerospace list-workspaces --all); do
     sketchybar --add item space.$sid left \
         --subscribe space.$sid aerospace_workspace_change \
         --set space.$sid \
-        background.color=$ITEM_BG_COLOR_ONE \
+        background.color=$ITEM_BG_COLOR_FIVE \
         background.corner_radius=15 \
         background.height=26 \
         background.drawing=off \
         label="$sid" \
-        label.color=$ITEM_BG_COLOR_TREE \
+        label.color=$TEXT_COLOR \
         icon.padding_left=0                          \
         icon.padding_right=14                          \
         label.padding_right=20                        \
