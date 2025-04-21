922ece5d5d308b0f5066c8b1e30c0c786969b1ce
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-04-21 17:03:41 +0300

    change colors

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -7,12 +7,20 @@ export TEXT_COLOR=0xff1e1e2e
 # export BAR_COLOR=0xff1f2430
 export BAR_COLOR=0x801e2330
 
-export ITEM_BG_COLOR_ONE=0xff7aa2f7
-export ITEM_BG_COLOR_TWO=0xffff757f
-export ITEM_BG_COLOR_TREE=0xffc3e88d
-export ITEM_BG_COLOR_FOUR=0xffffc777
+# export ITEM_BG_COLOR_ONE=0xff7aa2f7
+# export ITEM_BG_COLOR_TWO=0xffff757f
+# export ITEM_BG_COLOR_TREE=0xffc3e88d
+# export ITEM_BG_COLOR_FOUR=0xffffc777
+# export ITEM_BG_COLOR_FIVE=0xffc0caf5
+# export ITEM_BG_COLOR_SIX=0xffbb9af7
+# export ITEM_BG_COLOR_SEVEN=0xff41a6b5
+
+export ITEM_BG_COLOR_ONE=0xffc0caf5
+export ITEM_BG_COLOR_TWO=0xffc0caf5
+export ITEM_BG_COLOR_TREE=0xffc0caf5
+export ITEM_BG_COLOR_FOUR=0xffc0caf5
 export ITEM_BG_COLOR_FIVE=0xffc0caf5
-export ITEM_BG_COLOR_SIX=0xffbb9af7
-export ITEM_BG_COLOR_SEVEN=0xff41a6b5
+export ITEM_BG_COLOR_SIX=0xffc0caf5
+export ITEM_BG_COLOR_SEVEN=0xffc0caf5
 
 export ACCENT_COLOR=0xffff9e64
