b4e6913ee0c5bb71a620eb5ce2daf23ae5aa3b40
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-05-04 19:58:03 +0300

    change time

--- sketchybar/plugins/calendar.sh
+++ sketchybar/plugins/calendar.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # sketchybar --set $NAME label="$(date +'%a %d %b %H:%M')"
-sketchybar --set $NAME label="$(LANG=ja_JP.UTF-8 date +'%m月%d日 (%a) %H:%M')"
+sketchybar --set $NAME label="$(LANG=ja_JP.UTF-8 date +'%d日%m月 (%a) %H:%M')"
