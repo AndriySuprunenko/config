a47b6ed727cdbf0c2718d432be58e55a66d5402b
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-07-01 19:58:00 +0300

    smth

--- sketchybar/items/brew.sh
+++ sketchybar/items/brew.sh
@@ -2,10 +2,9 @@
 
 sketchybar --add item brew left \
            --set brew \
-             icon="Brew" \
+             icon="Brew packages:" \
              icon.padding_left=10 \
-             icon.padding_right=0 \
+             icon.padding_right=10 \
              background.color=$ITEM_BG_COLOR_FOUR \
-             label.padding_left=10 \
              update_freq=1800 \
              script="$HOME/.config/sketchybar/plugins/brew.sh"
